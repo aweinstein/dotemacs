f5fc5d9ab8281f47a61e64f19a6ba1638087018c
aweinstein <alejandr.weinstein@gmail.com>
2012-01-10 13:55:13 -0700

    Added latex setup to org mode.

--- org-config.el
+++ org-config.el
@@ -8,3 +8,20 @@
 ;    To save the clock history across Emacs sessions, use 
 (setq org-clock-persist 'history)
 (org-clock-persistence-insinuate)
+
+; Latex configuration
+; See http://orgmode.org/worg/org-tutorials/org-latex-export.html
+(require 'org-latex)
+(unless (boundp 'org-export-latex-classes)
+  (setq org-export-latex-classes nil))
+(add-to-list 'org-export-latex-classes
+	     '("article"
+                "\\documentclass[10pt,letterpaper]{article}
+		\\usepackage[T1]{fontenc}
+		\\usepackage{graphicx}
+		\\usepackage{geometry}
+		\\geometry{left=1.5in, right=2in, top=2.5cm,bottom=4 cm}
+		\\pagestyle{empty}"
+
+                ("\\section{%s}" . "\\section*{%s}")))
+
