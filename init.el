9a599d5cb66875810332baba7fe5ff89f7ce1739
aweinstein <alejandro.weinstein@gmail.com>
2012-10-02 06:41:18 -0600

    Fix python config.

--- init.el
+++ init.el
@@ -19,3 +19,15 @@
 ;(require 'pomodoro)
 
 (message "* --[ Done with AJW Emacs init file ]--")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((Local . castellano)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- python-config.el
+++ python-config.el
@@ -15,10 +15,11 @@
 (setq py-force-py-shell-name-p t)
 
 ; switch to the interpreter after executing code
-(setq py-shell-switch-buffers-on-execute-p t)
-(setq py-switch-buffers-on-execute-p t)
+; (setq py-shell-switch-buffers-on-execute-p t)
+; (setq py-switch-buffers-on-execute-p t)
+
 ; don't split windows
-(setq py-split-windows-on-execute-p nil)
+; (setq py-split-windows-on-execute-p nil)
 ; try to automagically figure out indentation
 (setq py-smart-indentation t)
 
