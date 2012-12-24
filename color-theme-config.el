f85c01a1ced6cec88171ae1340d9548cbefc9497
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2012-12-24 11:48:19 -0700

    Show host in tramp mode.

--- color-theme-config.el
+++ color-theme-config.el
@@ -2,9 +2,6 @@
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
-;(add-to-list 'load-path "~/.emacs.d/themes")
-;(require 'color-theme-zenburn)
-;(color-theme-zenburn)
 
-; For ReST mode with dark background
+;For ReST mode with dark background
 (setq frame-background-mode 'dark)
--- tramp-config.el
+++ tramp-config.el
@@ -1,4 +1,31 @@
-; use sudo through tramp
-; from http://stackoverflow.com/questions/3465567/how-to-use-ssh-and-sudo-together-with-tramp-in-emacs
-; open the remote file as C-x C-f /sudo:root@host[#port]:/path/to/file
-(set-default 'tramp-default-proxies-alist (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
+;http://stackoverflow.com/questions/715855/how-can-i-use-emacs-tramp-to-double-hop-ssh
+;open a file as
+; /ssh:ajw@shiner.mines.edu:
+(require 'tramp)
+(add-to-list 'tramp-default-proxies-alist
+ 	     '("shiner.mines.edu" nil "/ssh:aweinste@imagine.mines.edu:"))
+
+; Show host name
+; http://www.gnu.org/software/emacs/manual/html_node/tramp/Frequently-Asked-Questions.html
+(defconst my-mode-line-buffer-identification
+  (list
+   '(:eval
+     (let ((host-name
+	    (or (file-remote-p default-directory 'host)
+		(system-name))))
+       (if (string-match "^[^0-9][^.]*\\(\\..*\\)" host-name)
+	   (substring host-name 0 (match-beginning 1))
+	 host-name)))
+   ": %12b"))
+
+(setq-default
+ mode-line-buffer-identification
+ my-mode-line-buffer-identification)
+
+
+(add-hook
+ 'dired-mode-hook
+ '(lambda ()
+    (setq
+     mode-line-buffer-identification
+     my-mode-line-buffer-identification)))
