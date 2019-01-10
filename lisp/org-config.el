30c6fc67f9ef29e074e877b61768386eee19f474
aweinstein <alejandro.weinstein@gmail.com>
2019-01-10 20:51:36 -0300

    Remove auto fill minor mode for org-mode.

--- lisp/org-config.el
+++ lisp/org-config.el
@@ -5,7 +5,7 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-(add-hook 'org-mode-hook 'turn-on-auto-fill)
+
 ;    To save the clock history across Emacs sessions, use 
 (setq org-clock-persist 'history)
 (org-clock-persistence-insinuate)
