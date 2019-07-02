436fcd058b682f6d7add60dcb845361cc45b5be0
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2019-07-02 15:00:28 -0400

    Use monokai theme.

--- lisp/color-theme-config.el
+++ lisp/color-theme-config.el
@@ -1,8 +1,12 @@
 ; Use zenburn colortheme: https://github.com/bbatsov/zenburn-emacs
 ; $ git clone https://github.com/bbatsov/zenburn-emacs ~/.emacs.d/themes/
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'zenburn t)
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+;; (load-theme 'zenburn t)
 
 ;For ReST mode with dark background
 (setq frame-background-mode 'dark)
+
+;; Monokai
+;; M-x package-install monokai-theme for the first time
+(load-theme 'monokai t)
