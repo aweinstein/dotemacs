ae443fa5648a321c1e37a0be352f226031588809
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2019-10-25 15:29:36 -0300

    Add GNU ELPA repository.

--- lisp/package-config.el
+++ lisp/package-config.el
@@ -4,9 +4,12 @@
   (require 'package)
   (package-initialize)
   (add-to-list 'package-archives 
-	       '("melpa" . "http://melpa.milkbox.net/packages/") 
+	       '("melpa" . "http://melpa.milkbox.net/packages/")
 	       t)
   (add-to-list 'package-archives
 	       '("elpy" . "http://jorgenschaefer.github.io/packages/")
 	       )
+  (add-to-list 'package-archives
+  	       '("gnu" . "http://elpa.gnu.org/packages/")
+  	       )
   )
