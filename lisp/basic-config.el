f59650a643e3538d8bdf77cb3f0dc387b25ec1b3
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2021-03-26 08:44:11 -0300

    Add basic config.

--- lisp/basic-config.el
+++ lisp/basic-config.el
@@ -1,5 +1,46 @@
 (setq inhibit-startup-message t)
+(tool-bar-mode -1) ; hide the toolbar
+(setq make-backup-files nil) ; Disable creation of backup files
+(fset 'yes-or-no-p 'y-or-n-p)
 
+;; Scrolling
+(setq scroll-margin 0)
+(setq scroll-conservatively 100000)
+(setq scroll-preserve-screen-position 1)
+
+;; Enable Delete Selection mode to replace a region just by typing text, and
+;; kill the selected text just by hitting the Backspace key.
+(delete-selection-mode 1)
+
+;; Enable column numbering
+(column-number-mode 1)
+
+;; Enable show parenthesis mode
+(show-paren-mode 1)
+
+;; Set column width to 79 characters
+(setq-default fill-column 79)
+
+;; Update files modified externally
+;; See http://www.emacswiki.org/emacs/RevertBuffer
+(global-auto-revert-mode t)
+
+;; Save customization configuration in a file differente than init.el
+(setq custom-file "~/.emacs.d/lisp/.emacs-custom.el")
+
+;; Always use english for dates
+(setq system-time-locale "C")
+
+;;;;;;;;;;;;;;;;;;; Key Bindings
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+;; org-mode
+(global-set-key (kbd "<f12>") 'org-capture)
+;; Since M-Tab is used by the OS, we remap it here
+(global-set-key (kbd "C-x t") 'pcomplete)
+
+
+;; Some useful packages
 (use-package try
 :ensure t)
 
@@ -7,3 +48,40 @@
 :ensure t
 :config
 (which-key-mode))
+
+;; on the fly syntax checking
+(use-package flycheck
+:ensure t
+:init
+(global-flycheck-mode t))
+
+;; snippets and snippet expansion
+(use-package yasnippet
+:ensure t
+:init
+(yas-global-mode 1))
+
+;;;;; From old basic-config.el
+
+
+;; ; Set google-chrome as default browser
+;; ; Should we use w3m?
+;; (setq browse-url-browser-function 'browse-url-generic
+;;       browse-url-generic-program "google-chrome")
+
+;; ;(load custom-file)
+
+;; (when (memq window-system '(x mac ns))
+;;   (exec-path-from-shell-initialize))
+
+;; ; Enable spell check on text mode
+;; (add-hook 'text-mode-hook 'flyspell-mode) ;turn on flyspell mode by default
+;; (add-hook 'text-mode-hook 'turn-on-flyspell)
+;; (add-hook 'text-mode-hook 'visual-line-mode)
+
+;; ; Enable pdf-tools
+;; ;(pdf-tools-install)
+
+;; (defalias 'list-buffers 'ibuffer)
+
+
