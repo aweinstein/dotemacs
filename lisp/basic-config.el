7cc5dd50c29086aec3a48d618001b2f68eda4c44
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2021-07-27 15:34:07 -0400

    Add yassnipet-snippets, and fix some indenting.

--- lisp/basic-config.el
+++ lisp/basic-config.el
@@ -41,24 +41,27 @@
 
 ;; Some useful packages
 (use-package try
-:ensure t)
+  :ensure t)
 
 (use-package which-key
-:ensure t
-:config
-(which-key-mode))
+  :ensure t
+  :config
+  (which-key-mode))
 
 ;; on the fly syntax checking
 (use-package flycheck
-:ensure t
-:init
-(global-flycheck-mode t))
+  :ensure t
+  :init
+  (global-flycheck-mode t))
 
 ;; snippets and snippet expansion
 (use-package yasnippet
-:ensure t
-:init
-(yas-global-mode 1))
+  :ensure t
+  :config
+  (use-package yasnippet-snippets
+    :ensure t)
+  :init
+  (yas-global-mode 1))
 
 ;; Spaceline
 (use-package spaceline
@@ -73,28 +76,28 @@
   )
 
 (use-package swiper
-:ensure try
-:config
-(progn
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-load-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-))
+  :ensure try
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (global-set-key "\C-s" 'swiper)
+    (global-set-key (kbd "C-c C-r") 'ivy-resume)
+    (global-set-key (kbd "<f6>") 'ivy-resume)
+    (global-set-key (kbd "M-x") 'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+    (global-set-key (kbd "<f1> l") 'counsel-load-library)
+    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+    (global-set-key (kbd "C-c g") 'counsel-git)
+    (global-set-key (kbd "C-c j") 'counsel-git-grep)
+    (global-set-key (kbd "C-c k") 'counsel-ag)
+    (global-set-key (kbd "C-x l") 'counsel-locate)
+    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+    ))
 
 ;; Muestra el reloj en formato 24 hrs
 (setq display-time-24hr-format t) ; Muestra el reloj en formato 24 hrs
