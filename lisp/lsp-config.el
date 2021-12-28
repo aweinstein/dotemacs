648b7d565f688eac4697b0880b7b8c4f60b1fb9a
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2021-12-28 15:20:43 -0300

    Add extra packagages for LSP.

--- lisp/lsp-config.el
+++ lisp/lsp-config.el
@@ -1,6 +1,7 @@
 ;; From https://github.com/rememberYou/.emacs.d/blob/master/config.org#lsp
 
 (use-package lsp-mode
+  :ensure t
   :commands (lsp lsp-deferred)
   :hook (
 	 ;; (prog-mode . lsp-deferred)
@@ -13,10 +14,12 @@
   ;; (lsp-session-file (expand-file-name (format "%s/emacs/lsp-session-v1"  xdg-data)))
   (read-process-output-max (* 1024 1024)))
 
-;; (use-package lsp-ui
-;;   :hook (lsp-mode . lsp-ui-mode))
+(use-package lsp-ui
+  :ensure t
+  :hook (lsp-mode . lsp-ui-mode))
 
 ;; When using lsp, it is likely that you will encounter programming errors. To navigate through these errors via the minibuffer, you can use a package for that. If like me, you use consult with your minibuffer completion, then consult-lsp is made for you.
 
-;; (use-package consult-lsp
-;;   :commands (consult-lsp-diagnostics consult-lsp-symbols))
+(use-package consult-lsp
+  :ensure t
+  :commands (consult-lsp-diagnostics consult-lsp-symbols))
