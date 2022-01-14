275e983c986ee70d43eab6674fa9baa6e3473609
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2022-01-14 15:14:48 -0300

    Use the afternoon theme

--- lisp/color-theme-config.el
+++ lisp/color-theme-config.el
@@ -1,6 +1,11 @@
-(use-package autumn-light-theme
-:ensure t
-:config (load-theme 'autumn-light t))
+;; (use-package autumn-light-theme
+;;   :ensure t
+;;   :config (load-theme 'autumn-light t)
+;;   )
+
+(use-package afternoon-theme
+  :ensure t
+  :config (load-theme 'afternoon t))
 
 ;; Otros temas para probar
 ;; Moe
