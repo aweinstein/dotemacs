2bebc69d0bb3ceeefe51b1d245feb502b9de9e41
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2022-01-19 17:44:45 -0300

    Avoid annoyng pop up windowns behavior.

--- lisp/basic-config.el
+++ lisp/basic-config.el
@@ -140,3 +140,11 @@
 
 (winner-mode)
 
+;; Prefer to reuse existing windows, especially those showing a buffer
+;; of the same mode
+;; From https://github.com/daviwil/emacs-from-scratch/blob/master/show-notes/Emacs-Tips-DisplayBuffer-1.org
+(setq display-buffer-base-action
+  '((display-buffer-reuse-window
+     display-buffer-reuse-mode-window
+     display-buffer-same-window
+     display-buffer-in-previous-window)))
