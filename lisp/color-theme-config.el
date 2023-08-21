d210cdcbc8b8fb6204a06a5f499f9b38ea6cb7aa
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2023-08-21 10:09:41 -0400

    Use modus-vivendi theme.

--- lisp/color-theme-config.el
+++ lisp/color-theme-config.el
@@ -1,14 +1,10 @@
-;; (use-package autumn-light-theme
-;;   :ensure t
-;;   :config (load-theme 'autumn-light t)
-;;   )
-
-(use-package afternoon-theme
+(use-package emacs
   :ensure t
-  :config (load-theme 'afternoon t))
+  :config (load-theme 'modus-vivendi t))
 
 ;; Otros temas para probar
 ;; Moe
 ;; Nimbus
 ;; Material
+;; Afternoon
 ;; Ver https://pawelbx.github.io/emacs-theme-gallery/ para mas opciones
