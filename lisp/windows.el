e57e271168148d76f2d3dd9149c5b181417f6c6e
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2026-06-03 09:18:05 -0400

    Adjust agenda file in winwdos machine depending of the machine.

--- lisp/windows.el
+++ lisp/windows.el
@@ -4,5 +4,8 @@
          (getenv "PATH")))
 
 ; Set the agenda files
-(setq org-agenda-files (list "c:/Users/ajw/Dropbox/org/agenda.org"))
+(setq org-agenda-files
+      (list (if (string-equal (system-name) "DESKTOP-S58VG48")
+                "c:/Users/Alejandro Weinstein/Dropbox/org/agenda.org"
+              "c:/Users/ajw/Dropbox/org/agenda.org")))
 
